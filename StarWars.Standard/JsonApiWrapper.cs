31038661ccd04e6e6c23d95dc255457b4a7d2add
Neil Sorensen <neil-sorensen@pluralsight.com>
2017-11-09 11:26:14 -0700

    StarWars is building as netstandard1.4

---
+++ StarWars.Standard/JsonApiWrapper.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace StarWars
+{
+    public class JsonApiWrapper
+    {
+        public T Get<T>(string url)
+        {
+            var client = new HttpClient();
+            using (var response = client.GetAsync(url).Result)
+            {
+                var stringContent = response.Content.ReadAsStringAsync().Result;
+                return JsonConvert.DeserializeObject<T>(stringContent);
+            }
+        }
+    }
+}
---
+++ StarWars.Standard/Person.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StarWars
+{
+    public class Person
+    {
+        public string Name { get; set; }
+        public string BirthYear { get; set; }
+        public string EyeColor { get; set; }
+        public string HairColor { get; set; }
+        public string Height { get; set; }
+        public string Mass { get; set; }
+        public string Url { get; set; }
+
+        public int Id
+        {
+            get
+            {
+                var identifierStart = Url.LastIndexOf("/", Url.Length - 2) + 1;
+                var idString = Url.Substring(identifierStart, (Url.Length - 1) - identifierStart);
+                return int.Parse(idString);
+            }
+        }
+    }
+}
