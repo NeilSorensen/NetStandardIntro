f8bf40338435cc5d0f1da77efdb944abd5824d4f
Neil Sorensen <neil-sorensen@pluralsight.com>
2017-11-09 11:51:09 -0700

    FizzBuzz.Tests work with netstandard2.0

---
+++ FizzBuzz.Standard/FizzBuzz.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+
+namespace FizzBuzz
+{
+    public class FizzBuzz
+    {
+        private List<Rule> _rules;
+
+        public FizzBuzz()
+        {
+            _rules = new List<Rule>
+            {
+                new Rule(x => x % 3 == 0, "Fizz"),
+                new Rule(x => x % 5 == 0, "Buzz")
+            };
+        }
+
+        public FizzBuzz(List<Rule> list)
+        {
+            _rules = list;
+        }
+
+        public string Translate(int input)
+        {
+            string result = "";
+            foreach (var rule in _rules)
+            {
+                if (rule.MatchChecker(input)) result += rule.Word;
+            }
+
+            if (string.IsNullOrEmpty(result))
+            {
+                result = input.ToString();
+            }
+            return result;
+        }
+    }
+}
---
+++ FizzBuzz.Standard/Rule.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace FizzBuzz
+{
+    public class Rule
+    {
+        public Rule(Func<int, bool> rule, string matchingWord)
+        {
+            Word = matchingWord;
+            MatchChecker = rule;
+        }
+        public Func<int, bool> MatchChecker { get; }
+        public string Word { get; }
+
+        public static Rule Parse(string serialized)
+        {
+            var parts = serialized.Split(new[] {"=>", ","}, StringSplitOptions.RemoveEmptyEntries);
+            var factor = int.Parse(parts[0]);
+            return new Rule(x => x % factor == 0 , parts[1]);
+        }
+    }
+}
