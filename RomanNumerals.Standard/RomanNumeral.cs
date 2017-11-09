cb77be8efc4e6a06b410061d0103771b2112c7f9
Neil Sorensen <neil-sorensen@pluralsight.com>
2017-11-09 11:19:57 -0700

    Basic .NET Standard project working

---
+++ RomanNumerals.Standard/RomanNumeral.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RomanNumerals
+{
+    public class RomanNumeral
+    {
+        private List<Tuple<int, string>> romanTokens = new List<Tuple<int, string>>
+        {
+            new Tuple<int, string>(1000, "M"),
+            new Tuple<int, string>(500, "D"),
+            new Tuple<int, string>(100, "C"),
+            new Tuple<int, string>(50, "L"),
+            new Tuple<int, string>(40, "XL"),
+            new Tuple<int, string>(10, "X"),
+            new Tuple<int, string>(9, "IX"),
+            new Tuple<int, string>(5, "V"),
+            new Tuple<int, string>(4, "IV"),
+            new Tuple<int, string>(1, "I"),
+        };
+
+        public string Convert(int value)
+        {
+            StringBuilder roman = new StringBuilder();
+            foreach (var token in romanTokens)
+            {
+                while (token.Item1 <= value)
+                {
+                    roman.Append(token.Item2);
+                    value -= token.Item1;
+                }
+            }
+            return roman.ToString();
+        }
+    }
+}
